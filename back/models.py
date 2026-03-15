2a2d300d9409af499b6d9fd98ed7b7fbb0c44b97
murad-2007ML <haliqhuseynov2000@gmail.com>
2026-03-15 13:53:54 +0400

    back changes done

--- back/models.py
+++ back/models.py
@@ -1,3 +1,4 @@
+
 from datetime import datetime
 from typing import Optional
 from sqlmodel import Field, SQLModel
@@ -8,6 +9,7 @@ class User(SQLModel, table=True):
     role: str  # "user" or "company"
     reputation: int = 0
     bio: Optional[str] = None
+    password_hash: Optional[str] = None
 
 class Task(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
